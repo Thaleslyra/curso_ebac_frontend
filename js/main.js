a3d65e6fc1c0979a859295bd2ad6b7fb73ccc2df
Thales André <thalespipa@hotmail.com>
2023-04-01 07:55:50 -0300

    initial create

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const  form = document.getElementById('form');
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+})
+
+$(document).ready(function () {
+   $("#telefone").mask("(00) 0000-0000");
+   $('#cpf').mask("000.000.000-00");
+   $('#cep').mask("00000-000");
+   
+    
+});
